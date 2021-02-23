00ceea3ff5c8e10f58ea06eadb42538ed1f308f3
dangtdn <nguyendanglop93@gmail.com>
2021-02-23 14:16:59 +0700

    'updatedReply'

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <div class="container mt-5">
+            <div class="container my-5">
                 <div class="row">
                     <div class="col-12 text-center">
                         © 2020 Your name, devchallenges.io
--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,92 +1,129 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
 import Footer from '../../components/Footer';
 import MenuClone from '../../components/Main/components/MenuClone';
 import Posts from '../../components/Main/components/Posts';
 
 const Explore = (props) => {
 
+    const [data, setData] = useState({
+        value: 1,
+        status: 'Everyone can reply'
+    })
+
+    const [dataImg, setDataImg] = useState([]);
+
+    const changeStatus = (value) => {
+        if(value === 1){
+            setData({
+                value: 1,
+                status: 'Everyone can reply',
+            })
+        }else{
+            setData({
+                value: 2,
+                status: 'People can reply',
+            })
+        }
+        document.querySelector('.post-form-panel').classList.remove('active');
+    }
+
     useEffect(() => {
         document.title = "Explore"
     }, []);
+
+    const clickHidden = () => {
+        document.querySelector('.post-form-panel').classList.toggle('active');
+    }
+
+    const changeFile = (e) => {
+        // let arrImg = e.target.files.map((item,index) => {
+        //     return item.name
+        // })
+        let arrImg = Array.from(e.target.files)
+        
+        setDataImg(arrImg);
+        
+        console.log(arrImg);
+    }
 
     return (
         <div>
             <div className="main">
                 <div className="container mt-4">
-                    <div className="row">   
+                    <div className="row">
                         <div className="col-md-9 order-md-0 order-1">
-                            <div class="post-form border-radius box-shadow mt-md-0 mt-3">
-                                <h5 class="color-black">Tweet something</h5>
+                            <div className="post-form border-radius box-shadow mt-md-0 mt-3">
+                                <h5 className="color-black">Tweet something</h5>
                                 <hr />
-                                <div class="post-comment-box d-flex flex-wrap">
-                                    <div class="user-img">
-                                        <img class="border-radius" src="images/user.jpg" alt="Image of User" />
+                                <div className="post-comment-box d-flex flex-wrap">
+                                    <div className="user-img">
+                                        <img className="border-radius" src="images/user.jpg" alt="Image of User" />
                                     </div>
-                                    <div class="form-group flex-fill w-50 border-radius overflow-hidden">
-                                        <div class="input-group">
-                                            <textarea class="form-control shadow-none py-3" cols="30" rows="3" placeholder="What's something?"></textarea>
+                                    <div className="form-group flex-fill w-50 border-radius overflow-hidden">
+                                        <div className="input-group">
+                                            <textarea className="form-control shadow-none py-3" cols={30} rows={3} placeholder="What's something?" defaultValue={""} />
                                         </div>
                                     </div>
                                 </div>
-                                <div class="post-form-control d-flex flex-wrap mt-3">
-                                    <div class="post-form-control-select d-flex flex-wrap">
-                                        <label for="inputFile"><i class='bx bxs-image-alt' ></i></label>
-                                        <input id="inputFile" type="file" multiple class="form-control d-none" />
-                                        <div class="post-form-control-comunity mx-3">
-                                            <span class="border-radius">
-                                                <i class='bx bx-globe'></i> Everyone can reply
-                                            </span>
-                                            <div class="user-panel post-form-panel box-shadow border-radius active">
-                                                <h5 class="color-black">Who can reply?</h5>
+                                <div className="post-form-control d-flex flex-wrap mt-3">
+                                    <div className="post-form-control-select d-flex flex-wrap">
+                                        <label htmlFor="inputFile"><i className="bx bxs-image-alt" /></label>
+                                        <input id="inputFile" type="file" multiple className="form-control d-none" onChange={changeFile}/>
+                                        <div className="post-form-control-comunity mx-3">
+                                            <span className="status border-radius" onClick={() => clickHidden()}>
+                                                <i className="bx bx-globe" /> {data.status  }
+                  </span>
+                                            <div className="user-panel post-form-panel box-shadow border-radius">
+                                                <h5 className="color-black">Who can reply?</h5>
                                                 <h6>Choose who can reply to this Tweet?</h6>
                                                 <ul>
                                                     <li>
-                                                        <a href=""><i class='bx bx-globe'></i> Everyone</a>
+                                                        <a href='#' onClick={() => {changeStatus(1)}}><i className="bx bx-globe"/> Everyone</a>
                                                     </li>
                                                     <li>
-                                                        <a href=""><i class='bx bxs-group' ></i> People you follow</a>
+                                                        <a href='#' onClick={() => {changeStatus(2)}}><i className="bx bxs-group" /> People you follow</a>
                                                     </li>
                                                 </ul>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="post-form-control-btn flex-fill w- text-end">
-                                        <button class="btn btn-post-form btn-border-radius">Tweet</button>
+                                    <div className="post-form-control-btn flex-fill w- text-end">
+                                        <button className="btn btn-post-form btn-border-radius">Tweet</button>
                                     </div>
                                 </div>
                             </div>
-                            <Posts />
-                        </div>
+                            <Posts /></div>
                         <div className="col-md-3 order-md-1 order-0">
                             <div className="vertical-trending border-radius box-shadow mt-3 mt-md-0">
                                 <h5 className="color-black">Trends for you</h5>
                                 <hr />
                                 <ul>
                                     <li>
-                                        <a href="">#programming</a>
+                                        <a href>#programming</a>
                                         <h5>213k Tweets</h5>
                                     </li>
                                     <li>
-                                        <a href="">#devchallenges</a>
+                                        <a href>#devchallenges</a>
                                         <h5>213k Tweets</h5>
                                     </li>
                                     <li>
-                                        <a href="">#frontend</a>
+                                        <a href>#frontend</a>
                                         <h5>213k Tweets</h5>
                                     </li>
                                     <li>
-                                        <a href="">#helsinki</a>
+                                        <a href>#helsinki</a>
                                         <h5>213k Tweets</h5>
                                     </li>
                                 </ul>
                             </div>
-                        </div> 
+                        </div>
                         <MenuClone />
-                    </div>
+                        </div>
                 </div>
             </div>
             <Footer />
-        </div>
+            </div>
+
     );
 };
 
